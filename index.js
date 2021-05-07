9cd77c593ba057f420558525107671bd36a0ea87
tony <tony11tony11t@gmail.com>
2021-05-07 10:11:55 +0800

    add download csv file function

--- index.js
+++ index.js
@@ -2,16 +2,25 @@
 let table = document.getElementById("main");
 let tbody = table.querySelector("tbody");
 let url = window.location.search.split("url=")[1];
+let csvText = "姓名,入會日期,介紹人,關係,居住地,職業,原因,付費學員,一般點名次數,特殊點名次數,總點名次數\n";
 
 var xhr = new XMLHttpRequest();
 xhr.open("GET", url);
 xhr.onload = e => {
+    let data = null;
 
-    let data = JSON.parse(e.target.responseText);
+    try{
+         data = JSON.parse(e.target.responseText);
+    }catch{
+        table.innerHTML = "<h1>無效網頁，請確認連結是否正確</h1>";
+    }
+
 
     Object.keys(data).forEach(k => {
         let tr = `<tr><td colspan="11" class="group">${data[k].name}</td></tr>`;
         tbody.insertAdjacentHTML("beforeend" , tr);
+
+        csvText += data[k].name + "\n";
     
         let students = data[k].student;
         Object.keys(students).forEach(s => {
@@ -29,10 +38,37 @@ xhr.onload = e => {
                         <td class="number">${students[s].specialNum + students[s].normalNum}</td>
                     </tr>`;
             tbody.insertAdjacentHTML("beforeend" , tr);
+
+            csvText += `${students[s].name},${students[s].startDate},${students[s].introducer},${students[s].relationship},${students[s].city},${students[s].career},${students[s].reason.replace(/\n/g,' ')},${students[s].money === "true" ? "O" : ""}, ${students[s].normalNum},${students[s].specialNum},${students[s].specialNum + students[s].normalNum}\n`
         })
     })
+
+    let fileName = () => {
+        let date = new Date();
+        let y    = date.getFullYear();
+        let m    = date.getMonth() + 1;
+        let d    = date.getDate();
+        return `${y}-${m}-${d}`;
+    }
+
+    // Start file download.
+    download(`${fileName()}.csv`,csvText);
 }
 xhr.send(null);
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+  
+    element.style.display = 'none';
+    document.body.appendChild(element);
+  
+    element.click();
+  
+    document.body.removeChild(element);
+}
+
 
 
 
